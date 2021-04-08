6e938610d5334e64a906bba25a0c7e7996455628
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-08 17:44:10 +0530

    landing page 1

--- src/pages/landingPage/landing_page.tsx
+++ src/pages/landingPage/landing_page.tsx
@@ -1,24 +1,64 @@
 import './landing_page.css';
 export default function LandingPage(){
-    return <div className="section1 min-vh-100 w-100 d-flex align-items-center">
-        <div className="container" id="base">
-            <div className="row-fluid text-center">
-                <div className="col-fluid">
-                    <h1 className="display-3">Welcome To </h1>
-                    <h3 className="display-1">{SITE_META.appName}</h3>
+    return <div className='section1'>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Pooja Automobiles</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                <button type="button" className="btn btn-outline-light mr-3">HOME</button></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <button type="button" className="btn btn-outline-light mr-3">SERVICES</button></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <button type="button" className="btn btn-outline-light">CONTACT US</button></a>
+                        </li>
+                    </ul>
                 </div>
-                <br/>
-                    <div className="row-fluid">
-                        <div className="col-fluid">
-
-                                <button  type="button" className="btn btn-outline-dark" id="loginb">Login</button>
-
-                        </div>
-                        <br/>
-                            <div className="col-fluid">
-                                <button type="button" className="btn btn-dark" id="signupb">Signup</button>
-                            </div>
+            </div>
+        </nav>
+        <div className='section2'>
+            <div className='container text-wrap' id='base'>
+                <div className='row-fluid'>
+                    <strong className='display-4'>
+                        The Best Car Services Await You.
+                    </strong>
+                </div>
+                <div className='row-fluid'>
+                    <div className="dropdown">
+                        <button className="btn btn-light dropdown-toggle my-3" type="button" id="dropdownMenuButton1"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            SELECT YOUR CAR
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                     </div>
+                    <div className="dropdown">
+                        <button className="btn btn-light dropdown-toggle mb-3" type="button" id="dropdownMenuButton1"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                           LOCATION
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                    <div className='btn btn-outline-light'> GO </div>
+                </div>
+
             </div>
         </div>
     </div>
