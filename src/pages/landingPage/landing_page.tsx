1990e58ee561ed2a72f35375d552aa14cd5349f7
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-08 12:48:56 +0530

    landingpage

--- src/pages/landingPage/landing_page.tsx
+++ src/pages/landingPage/landing_page.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import SampleComponent from "./sampleComponent/sample_component";
+import './landing_page.css';
+export default function LandingPage(){
+    return <div className="section1 min-vh-100 w-100 d-flex align-items-center">
+        <div className="container" id="base">
+            <div className="row-fluid text-center">
+                <div className="col-fluid">
+                    <h1 className="display-3">Welcome To </h1>
+                    <h3 className="display-1">{SITE_META.appName}</h3>
+                </div>
+                <br/>
+                    <div className="row-fluid">
+                        <div className="col-fluid">
 
-function LandingPage() {
-    return (
-        <div>
-            <SampleComponent/>
-            Hello World
+                                <button  type="button" className="btn btn-outline-dark" id="loginb">Login</button>
+
+                        </div>
+                        <br/>
+                            <div className="col-fluid">
+                                <button type="button" className="btn btn-dark" id="signupb">Signup</button>
+                            </div>
+                    </div>
+            </div>
         </div>
-    );
+    </div>
 }
-
-export default LandingPage;
--- src/pages/landingPage/sampleComponent/sample_component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SampleComponent() {
-    return (
-        <div>THIS IS A SAMPLE COMPONENT</div>
-    );
-}
-
-export default SampleComponent;
