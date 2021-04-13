cd5f65bce9bab10f4eb765a575f2c18d4529b48f
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-13 23:46:02 +0530

    services page dev 2

--- src/pages/servicesPage/services_page.tsx
+++ src/pages/servicesPage/services_page.tsx
@@ -1,97 +1,109 @@
 import './services_page.css'
 
 export default function ServicesPage(){
-    return <div className='container-fluid justify-content-center align-content-center bg-info flex-grow-1' >
-                <div className='row-fluid bg-info d-flex flex-row '>
-                    <div className='col-md-6 bg-warning sticky-top' id='leftcol'>
+    return <div className='container-fluid justify-content-center align-content-center  flex-grow-1' >
+                <div className='row-fluid flex-wrap d-flex flex-row '>
+                    <div className='col-lg-6 sticky-top' id='leftcol'>
 
                         <div className='d-flex align-items-center justify-content-center flex-column' id='cart'>
-                            <div className="card  ">
+
+                            <h2 className='display-4'>CART</h2>
+                            <img className="img-fluid" src='unnamed.png'></img>
+                            <hr id='cartline'></hr>
+
+                            <div className="card">
                                 <h5 className="card-header">FRONT BUMPER</h5>
-                                <div className="card-body p-2 bg-danger d-flex flex-row flex-wrap">
-                                    <div className='col-md-10 bg-info d-flex justify-content-between align-items-center'>
+                                <div className="card-body p-2 d-flex flex-row flex-wrap">
+
+                                    <div className='col-lg-10 d-flex justify-content-between align-items-center'>
                                         <p className="card-text my-auto">2 year warranty on paint.</p>
-                                        <a href="#" className="btn btn-primary">Remove</a>
+                                        <a href="#" className="btn w-auto btn-outline-light">Remove</a>
                                     </div>
-                                    <div className='col-md-2 text-dark bg-dark'>
-                                        <h4 ><span className="badge  bg-light">Rs.4999</span></h4>
+                                    <div className='col-lg-2 d-flex align-items-center '>
+                                        <h4 className="my-auto mx-auto">Rs.4999</h4>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="card">
+                                <h5 className="card-header">REAR BUMPER</h5>
+                                <div className="card-body p-2 d-flex flex-row flex-wrap">
+                                    <div className='col-md-10 d-flex justify-content-between align-items-center'>
+                                        <p className="card-text my-auto">2 year warranty on paint.</p>
+                                        <a href="#" className="btn w-auto btn-outline-light">Remove</a>
+                                    </div>
+                                    <div className='col-md-2 d-flex align-items-center '>
+                                        <h4 className="my-auto mx-auto">Rs.4999</h4>
                                     </div>
 
                                 </div>
                             </div>
 
-                                <div className="card ">
-                                    <h5 className="card-header">Featured</h5>
-                                    <div className="card-body d-flex justify-content-between align-items-center">
-                                        <p className="card-text  text-wrap">With supporting text below as a natural lead-in to additional
-                                            content.</p>
-                                        <a href="#" className="btn btn-primary">Remove</a>
-                                    </div>
-                                </div>
-
-                            <hr/>
-
                         </div>
-
-
                     </div>
 
-                    <div className='col-md-6 bg-danger d-flex flex-column' id='rightcol'>
+                    <div className='col-lg-6 d-flex flex-column' id='rightcol'>
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
 
-                        <div className="card mt-5">
-                            <h5 className="card-header">Featured</h5>
+                            <h4 className="card-header">A REPAIR</h4>
                             <div className="card-body">
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional
                                     content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
-                        </div>
 
-                        <div className="card">
-                            <h5 className="card-header">Featured</h5>
-                            <div className="card-body">
-                                <h5 className="card-title">Special title treatment</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <h5 className="card-header">Featured</h5>
+                        </a>
+
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
+
+                            <h4 className="card-header">B REPAIR</h4>
                             <div className="card-body">
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional
                                     content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
-                        </div>
-                        <div className="card">
-                            <h5 className="card-header">Featured</h5>
+
+                        </a>
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
+
+                            <h4 className="card-header">C REPAIR</h4>
                             <div className="card-body">
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional
                                     content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
-                        </div>
-                        <div className="card">
-                            <h5 className="card-header">Featured</h5>
+
+                        </a>
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
+
+                            <h4 className="card-header">V REPAIR</h4>
                             <div className="card-body">
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional
                                     content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
-                        </div>
-                        <div className="card">
-                            <h5 className="card-header">Featured</h5>
+
+                        </a>
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
+
+                            <h4 className="card-header">D REPAIR</h4>
                             <div className="card-body">
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional
                                     content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
-                        </div>
+
+                        </a>
+                        <a href="#" className='btn btn-outline-light text-left w-auto mt-5'>
+
+                            <h4 className="card-header">F REPAIR</h4>
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional
+                                    content.</p>
+                            </div>
+
+                        </a>
                     </div>
                 </div>
             </div>
