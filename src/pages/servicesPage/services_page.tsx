57b31f74ee399154e583c917ab1fd7f4bb4bb983
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-14 11:48:48 +0530

    services page dev 3

--- src/pages/servicesPage/services_page.tsx
+++ src/pages/servicesPage/services_page.tsx
@@ -1,14 +1,19 @@
 import './services_page.css'
-
+import image from './unnamed.png'
 export default function ServicesPage(){
-    return <div className='container-fluid justify-content-center align-content-center  flex-grow-1' >
+    return <div className='container-fluid justify-content-center align-content-center flex-grow-1' >
                 <div className='row-fluid flex-wrap d-flex flex-row '>
-                    <div className='col-lg-6 sticky-top' id='leftcol'>
+                    <div className='col-lg-6 align-items-start sticky-top' id='leftcol'>
 
                         <div className='d-flex align-items-center justify-content-center flex-column' id='cart'>
 
                             <h2 className='display-4'>CART</h2>
-                            <img className="img-fluid" src='unnamed.png'></img>
+                            <img className="img-fluid w-50" src={image}></img>
+                            <hr id='cartline'></hr>
+                            <div className='col-12 d-flex  justify-content-between align-items-center '>
+                                <h2 className='display-6'>CAR NAME</h2>
+                                <a href="#" className="btn w-auto btn-outline-light">CHANGE</a>
+                            </div>
                             <hr id='cartline'></hr>
 
                             <div className="card">
@@ -38,6 +43,7 @@ export default function ServicesPage(){
 
                                 </div>
                             </div>
+
 
                         </div>
                     </div>
