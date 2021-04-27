3ff2d0067bb78a178b071834faba90b74afd8892
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-27 12:25:57 +0530

    eweffwe

--- src/pages/allservicespage/all_services_page.tsx
+++ src/pages/allservicespage/all_services_page.tsx
@@ -126,6 +126,9 @@ export default function AllServicesPage() {
                                 </Container>
                             ))}
                         </div>
+                        <Button variant='success'>
+                            CHECKOUT
+                        </Button>
                     </div>
                 </div>
 
