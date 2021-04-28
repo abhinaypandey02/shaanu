11dddfae447b8c54c6a0657106a2817f5c30ad25
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-28 21:49:57 +0530

    profile page

---
+++ src/pages/profilePage/profile_page.tsx
@@ -0,0 +1,43 @@
+export default function ProfilePage() {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-6">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="col-6">
+                    <img src='' className='img-fluid'/>
+                </div>
+            </div>
+        </div>
+    )
+}
