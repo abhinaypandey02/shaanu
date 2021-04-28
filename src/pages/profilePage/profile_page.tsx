fa0b97736183c279267a7e2dc02f56360b713d47
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-28 22:11:54 +0530

    fefef

--- src/pages/profilePage/profile_page.tsx
+++ src/pages/profilePage/profile_page.tsx
@@ -1,41 +1,40 @@
 export default function ProfilePage() {
     return (
         <div className="container-fluid">
+            <div className="row text-center mb-3">
+                <button className="btn mx-auto btn-light">
+                    CREATE NEW CAR PROFILE
+                </button>
+            </div>
+
             <div className="row">
-                <div className="col-6">
-                    <table className="table">
-                        <thead>
+                <div className="col-md-6">
+                    <table className="table text-light text-center table-bordered">
+                    <tbody>
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <th scope="col">CAR'S NAME</th>
+                                <td>MARUZIT USUKI MARU</td>
                             </tr>
                             <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
+                                <th scope="row">REGISTRATION NUMBER.</th>
+                                <td>2135315135135</td>
+
                             </tr>
                             <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
+                                <th scope="row">NEXT SERVICE DUE DATE</th>
+                                <td> 13/032/202123</td>
+
+                            </tr>
+                            <tr>
+                                <th scope="row">INSURANCE DUE DATE</th>
+                                <td>13/03/2212</td>
+
                             </tr>
                         </tbody>
                     </table>
                 </div>
-                <div className="col-6">
-                    <img src='' className='img-fluid'/>
+                <div className="col-md-6">
+                    <img src='https://images.unsplash.com/photo-1519440862171-af26cf8c2a85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80' className='img-fluid border rounded-lg'/>
                 </div>
             </div>
         </div>
