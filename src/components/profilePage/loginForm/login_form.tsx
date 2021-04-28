3c97cf083d70452b96f218ed5512003e260b6f7f
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-04-28 22:57:48 +0530

    forms

--- src/components/profilePage/loginForm/login_form.tsx
+++ src/components/profilePage/loginForm/login_form.tsx
@@ -1,14 +1,82 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { signInWithEmailAndPassword } from "../../../utils/firebase/auth";
 
+const ERROR_MESSAGES = {
+    empty: "This field cannot be empty",
+    shortPassword: "The password must be of atleast length 6",
+    invalidEmail: "This email is invalid",
+};
+function validateEmail(email: string) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 export default function LoginForm() {
+    const [email, setEmail] = useState({ value: "", error: "" });
+    const [password, setPassword] = useState({ value: "", error: "" });
+
+    function onSubmit(e: any) {
+        e.preventDefault();
+        setEmail(old=>({...old,error:''}))
+        setPassword(old=>({...old,error:''}))
+        if (email.value === "") {
+            setEmail({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (password.value === "") {
+            setEmail({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (password.value.length < 6) {
+            setPassword({
+                value: password.value,
+                error: ERROR_MESSAGES.shortPassword,
+            });
+            return;
+        }
+        if (!validateEmail(email.value)) {
+            setEmail((old) => ({ ...old, error: ERROR_MESSAGES.invalidEmail }));
+            return;
+        }
+        signInWithEmailAndPassword(email.value,password.value).then(()=>console.log("done"))
+    }
+
     return (
-        <Form>
+        <Form onSubmit={onSubmit} noValidate>
             <Form.Group>
-                <Form.Label>Email2</Form.Label>
-                <Form.Control type="email"/>
-                <Form.Text></Form.Text>
+                <Form.Label className="text-white">Email</Form.Label>
+                <Form.Control
+                    id="email"
+                    value={email.value}
+                    onChange={(e) =>
+                        setEmail((old) => ({ ...old, value: e.target.value }))
+                    }
+                    type="email"
+                />
+                <Form.Text className="small text-danger">
+                    {email.error}
+                </Form.Text>
             </Form.Group>
+            <Form.Group>
+                <Form.Label className="text-white">Password</Form.Label>
+                <Form.Control
+                    id="password"
+                    value={password.value}
+                    onChange={(e) =>
+                        setPassword((old) => ({
+                            ...old,
+                            value: e.target.value,
+                        }))
+                    }
+                    type="password"
+                />
+                <Form.Text className="small text-danger">
+                    {password.error}
+                </Form.Text>
+            </Form.Group>
+            <Button variant="outline-light" type="submit">
+                Log In
+            </Button>
         </Form>
-    )
+    );
 }
--- src/components/profilePage/registerForm/register_form.tsx
+++ src/components/profilePage/registerForm/register_form.tsx
@@ -1,14 +1,136 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import UserInterface from "../../../interfaces/user";
+import { signUpWithEmailAndPassword } from "../../../utils/firebase/auth";
+import { createUserDocument } from "../../../utils/firebase/firestore";
 
+const ERROR_MESSAGES = {
+    empty: "This field cannot be empty",
+    shortPassword: "The password must be of atleast length 6",
+    invalidEmail: "This email is invalid",
+};
+function validateEmail(email: string) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 export default function RegisterForm() {
+    const [firstName, setFirstName] = useState({ value: "", error: "" });
+    const [lastName, setLastName] = useState({ value: "", error: "" });
+    const [email, setEmail] = useState({ value: "", error: "" });
+    const [password, setPassword] = useState({ value: "", error: "" });
+
+    function onSubmit(e: any) {
+        e.preventDefault();
+        setFirstName(old=>({...old,error:''}))
+        setLastName(old=>({...old,error:''}))
+        setEmail(old=>({...old,error:''}))
+        setPassword(old=>({...old,error:''}))
+        if (firstName.value === "") {
+            setFirstName({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (lastName.value === "") {
+            setLastName({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (email.value === "") {
+            setEmail({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (password.value === "") {
+            setEmail({ value: "", error: ERROR_MESSAGES.empty });
+            return;
+        }
+        if (password.value.length < 6) {
+            setPassword({
+                value: password.value,
+                error: ERROR_MESSAGES.shortPassword,
+            });
+            return;
+        }
+        if (!validateEmail(email.value)) {
+            setEmail((old) => ({ ...old, error: ERROR_MESSAGES.invalidEmail }));
+            return;
+        }
+        const user:UserInterface={
+            email:email.value,firstName:firstName.value,lastName:lastName.value
+        }
+        signUpWithEmailAndPassword(email.value,password.value).then(()=>{
+            createUserDocument(user).then(()=>console.log("done"))
+        })
+        
+    }
+
     return (
-        <Form>
+        <Form onSubmit={onSubmit} noValidate>
             <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email"/>
-                <Form.Text></Form.Text>
+                <Form.Label className="text-white">First Name</Form.Label>
+                <Form.Control
+                    id="email"
+                    value={firstName.value}
+                    onChange={(e) =>
+                        setFirstName((old) => ({
+                            ...old,
+                            value: e.target.value,
+                        }))
+                    }
+                    type="text"
+                />
+                <Form.Text className="small text-danger">
+                    {firstName.error}
+                </Form.Text>
             </Form.Group>
+            <Form.Group>
+                <Form.Label className="text-white">Last Name</Form.Label>
+                <Form.Control
+                    id="email"
+                    value={lastName.value}
+                    onChange={(e) =>
+                        setLastName((old) => ({
+                            ...old,
+                            value: e.target.value,
+                        }))
+                    }
+                    type="text"
+                />
+                <Form.Text className="small text-danger">
+                    {lastName.error}
+                </Form.Text>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label className="text-white">Email</Form.Label>
+                <Form.Control
+                    id="email"
+                    value={email.value}
+                    onChange={(e) =>
+                        setEmail((old) => ({ ...old, value: e.target.value }))
+                    }
+                    type="email"
+                />
+                <Form.Text className="small text-danger">
+                    {email.error}
+                </Form.Text>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label className="text-white">Password</Form.Label>
+                <Form.Control
+                    id="password"
+                    value={password.value}
+                    onChange={(e) =>
+                        setPassword((old) => ({
+                            ...old,
+                            value: e.target.value,
+                        }))
+                    }
+                    type="password"
+                />
+                <Form.Text className="small text-danger">
+                    {password.error}
+                </Form.Text>
+            </Form.Group>
+            <Button variant="outline-light" type="submit">
+                Sign Up
+            </Button>
         </Form>
-    )
+    );
 }
