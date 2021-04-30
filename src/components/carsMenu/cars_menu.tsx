66a0273bb811cf0314038eb90a4c6c03bc6c9a5b
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-04-30 15:28:05 +0530

    adding images for fuel

--- src/components/carsMenu/cars_menu.tsx
+++ src/components/carsMenu/cars_menu.tsx
@@ -15,7 +15,7 @@ export default function CarsMenu() {
     const brands = getBrands();
 
     function setBrand(id: CarBrand["id"]) {
-        console.log(id)
+        console.log(id);
         dispatch({ type: "SET_BRAND", payload: id });
     }
     function setModel(id: CarModel["id"]) {
@@ -24,43 +24,68 @@ export default function CarsMenu() {
     function setType(type: string) {
         dispatch({ type: "SET_TYPE", payload: type });
     }
-    const selectedBrand=brands.find(brand=>brand.id===globalState.selectedBrand)
-    if (!globalState.selectedBrand||!selectedBrand) {
+    const selectedBrand = brands.find(
+        (brand) => brand.id === globalState.selectedBrand
+    );
+    if (!globalState.selectedBrand || !selectedBrand) {
         return (
             <div className="d-flex flex-wrap justify-content-around overflow-auto">
                 {brands.map((brand) => (
-                    <div className="text-center" style={{width:"33%"}}>
-                    <img alt={brand.name} height={80} className="pointer-on-hover" src={brand.imageURL} key={brand.id} onClick={() => setBrand(brand.id)}/>
-                    <div>{brand.name}</div>
-                   </div>
+                    <div className="text-center" style={{ width: "33%" }}>
+                        <img
+                            alt={brand.name}
+                            height={80}
+                            className="pointer-on-hover"
+                            src={brand.imageURL}
+                            key={brand.id}
+                            onClick={() => setBrand(brand.id)}
+                        />
+                        <div>{brand.name}</div>
+                    </div>
                 ))}
             </div>
         );
     }
-    const selectedModel=selectedBrand.models.find(model=>model.id===globalState.selectedModel)
-    if (!globalState.selectedModel||!selectedModel ) {
+    const selectedModel = selectedBrand.models.find(
+        (model) => model.id === globalState.selectedModel
+    );
+    if (!globalState.selectedModel || !selectedModel) {
         return (
             <div className="d-flex flex-wrap justify-content-around">
                 {selectedBrand.models.map((model) => (
-                    <div className="text-center" style={{width:"33%"}}>
-                    <img alt={model.name} className="pointer-on-hover" src={model.imageURL} key={model.id} onClick={() => setModel(model.id)}/>
-<div>{model.name} </div>
-                        </div>
-
-                        
-                    
+                    <div className="text-center" style={{ width: "33%" }}>
+                        <img
+                            alt={model.name}
+                            className="pointer-on-hover"
+                            src={model.imageURL}
+                            key={model.id}
+                            onClick={() => setModel(model.id)}
+                        />
+                        <div>{model.name} </div>
+                    </div>
                 ))}
             </div>
         );
     }
-    if (!globalState.selectedType ) {
+    if (!globalState.selectedType) {
         return (
             <div className="d-flex flex-wrap justify-content-around">
-                {selectedModel.type.map((type) => (
-                    <Button key={type} onClick={() => setType(type)}>
-                        {type}
-                    </Button>
-                ))}
+                {selectedModel.type.map((type) => {
+                    let imgURL="";
+                    switch(type){
+                        case "petrol":{imgURL="https://zeevector.com/wp-content/uploads/2021/02/Hindustan-Petroleum-Logo-PNG.png";break;}
+                        case "cng":{imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/CNG_logo_China.svg/1280px-CNG_logo_China.svg.png";break;}
+                        case "diesel":{imgURL="https://m.media-amazon.com/images/M/MV5BMjExNzA4MDYxN15BMl5BanBnXkFtZTcwOTI1MDAxOQ@@._V1_.jpg";break;}
+                    }
+                    return <div className="text-center" style={{ width: "33%" }}><img
+                        alt={type}
+                        height={80}
+                        className="pointer-on-hover"
+                        src={imgURL}
+                        key={type}
+                        onClick={() => setType(type)}
+                    /><div className="text-capitalize text-center">{type}</div></div>
+                })}
             </div>
         );
     }
