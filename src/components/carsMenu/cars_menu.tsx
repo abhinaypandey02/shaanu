a57a086b19ac1906a479a6ee70f1e43a4dd6e678
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-30 14:16:08 +0530

    dwasdw

--- src/components/carsMenu/cars_menu.tsx
+++ src/components/carsMenu/cars_menu.tsx
@@ -30,7 +30,7 @@ export default function CarsMenu() {
             <div className="d-flex flex-wrap justify-content-around">
                 {brands.map((brand) => (
                     <div>
-                    <img className="pointer-on-hover" src={brand.imageURL} key={brand.id} onClick={() => setBrand(brand.id)}/>
+                    <img className="pointer-on-hover" height={80} src={brand.imageURL} key={brand.id} onClick={() => setBrand(brand.id)}/>
                     <div>{brand.name}</div>
                    </div>
                 ))}
