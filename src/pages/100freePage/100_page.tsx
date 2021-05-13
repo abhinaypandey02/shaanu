4ab93035282c1d972b0e22d558dd42816d151e5b
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-05-13 18:32:36 +0530

    adding modal

--- src/pages/100freePage/100_page.tsx
+++ src/pages/100freePage/100_page.tsx
@@ -1,8 +1,18 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export default function FreeServices() {
+    const [showConfirmationModal,setShowConfirmationModal]=useState(false);
     return (
         <div className="container-fluid">
+            <Modal centered={true} show={showConfirmationModal} onHide={()=>setShowConfirmationModal(false)}>
+                <Modal.Header closeButton={true}>
+
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+            </Modal>
             <div className="row-fluid text-light text-center">
                 <h1>CHOOSE ANY FREE SERVICE</h1>
             </div>
@@ -162,7 +172,7 @@ export default function FreeServices() {
                     </div>
                 </div>
                 <div className="col-md-6 text-center">
-                    <Button variant='outline-light btn-lg m-3'>BOOK FREE APPOINTMENT</Button>
+                    <Button onClick={()=>setShowConfirmationModal(true)} variant='outline-light btn-lg m-3'>BOOK FREE APPOINTMENT</Button>
                 </div>
             </div>
         </div>
