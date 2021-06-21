4576a6ba78a1e76f08d890234ce11bb4f392e50d
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-06-21 19:01:13 +0530

    Update navigation_bar.tsx

--- src/components/navigationBar/navigation_bar.tsx
+++ src/components/navigationBar/navigation_bar.tsx
@@ -8,7 +8,7 @@ export default function NavigationBar() {
         <div className="div">
         <Navbar variant={"dark"} bg="dark" className='my-md-4' expand="lg">
         <Navbar.Brand href="#home" className='ml-md-3'>
-            <img className='img-fluid bg-warning border rounded-circle mr-2' width="50" height="50" src='https://images.vexels.com/media/users/3/144355/isolated/preview/0a53cf7ef3dcdc995f7f20c13c697a25-speed-car-lines-logo-by-vexels.png'/>
+            <img alt="logo" className='img-fluid bg-warning border rounded-circle mr-2' width="50" height="50" src='https://images.vexels.com/media/users/3/144355/isolated/preview/0a53cf7ef3dcdc995f7f20c13c697a25-speed-car-lines-logo-by-vexels.png'/>
             <strong>Car Plus</strong></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
