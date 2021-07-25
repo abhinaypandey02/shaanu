f0e81dbdd2e8c86e95177fd3a0132b4deba7c998
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-07-25 17:10:35 +0530

    optimized images

--- src/pages/reviewPage/reviewpage.tsx
+++ src/pages/reviewPage/reviewpage.tsx
@@ -108,14 +108,15 @@ export default function ReviewPage(){
 
                                  <div className="stars pl-0 ml-0">
                 <form action=""> <input className="star star-5" id="star-5" type="radio" name="star" />
-                 <label className="star star-5" htmlFor="star-5"></label> 
+                 <label className="star star-5" htmlFor="star-5"/>
                  <input className="star star-4" id="star-4" type="radio" name="star" /> 
-                 <label className="star star-4" htmlFor="star-4"></label> 
+                 <label className="star star-4" htmlFor="star-4"/>
                  <input className="star star-3" id="star-3" type="radio" name="star" /> 
-                 <label className="star star-3" htmlFor="star-3"></label>
+                 <label className="star star-3" htmlFor="star-3"/>
                   <input className="star star-2" id="star-2" type="radio" name="star" /> 
-                  <label className="star star-2" htmlFor="star-2"></label> <input className="star star-1" id="star-1" type="radio" name="star" /> 
-                  <label className="star star-1" htmlFor="star-1"></label> </form>
+                  <label className="star star-2" htmlFor="star-2"/>
+                    <input className="star star-1" id="star-1" type="radio" name="star" />
+                  <label className="star star-1" htmlFor="star-1"/> </form>
             </div>
 
                             </div>
