22e046790b8d2abea7f68bafabcff8013911cd28
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-08-14 12:13:36 +0530

    new radio

--- src/pages/100freePage/100_page.tsx
+++ src/pages/100freePage/100_page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import { useHistory } from 'react-router';
-
+import './100pages.css';
 export default function FreeServices() {
     const history = useHistory();
     return (
@@ -19,42 +19,34 @@ export default function FreeServices() {
                             <ul className="list-group border border-dark p-3 text-dark w-100 bg-dark rounded-0">
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios11" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">One
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios12" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">two
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios13"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">three
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios14" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">four
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
 
@@ -67,49 +59,41 @@ export default function FreeServices() {
                 <div className="col-md-3">
                     <div className="container-fluid p-3 d-flex flex-wrap flex-column">
                         <div className="row bg-warning">
-                            <h1 className='mx-auto my-2'>GROUP 2</h1>
+                            <h1 className='mx-auto my-2'>GROUP 1</h1>
                         </div>
                         
                         <div className="row border border-warning text-light">
                             <ul className="list-group border border-dark p-3 text-dark w-100 bg-dark rounded-0">
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios21"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">One
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios22" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">two
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios23" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">three
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios2" id="exampleRadios24"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">four
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
 
@@ -118,53 +102,44 @@ export default function FreeServices() {
                     </div>
 
                 </div>
-
                 <div className="col-md-3">
                     <div className="container-fluid p-3 d-flex flex-wrap flex-column">
                         <div className="row bg-warning">
-                            <h1 className='mx-auto my-2'>GROUP 3</h1>
+                            <h1 className='mx-auto my-2'>GROUP 1</h1>
                         </div>
                         
                         <div className="row border border-warning text-light">
                             <ul className="list-group border border-dark p-3 text-dark w-100 bg-dark rounded-0">
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios3" id="exampleRadios1"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">One
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios3" id="exampleRadios1" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">two
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios3" id="exampleRadios1"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">three
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios3" id="exampleRadios1"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">four
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
 
@@ -173,53 +148,44 @@ export default function FreeServices() {
                     </div>
 
                 </div>
-
                 <div className="col-md-3">
                     <div className="container-fluid p-3 d-flex flex-wrap flex-column">
                         <div className="row bg-warning">
-                            <h1 className='mx-auto my-2'>GROUP 4</h1>
+                            <h1 className='mx-auto my-2'>GROUP 1</h1>
                         </div>
                         
                         <div className="row border border-warning text-light">
                             <ul className="list-group border border-dark p-3 text-dark w-100 bg-dark rounded-0">
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios4" id="exampleRadios1" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">One
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios4" id="exampleRadios1" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">two
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios4" id="exampleRadios1" />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">three
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
                                 <li className="list-group-item border border-dark bg-warning">
                                     <div className="col-12 my-2">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name="exampleRadios4" id="exampleRadios1"  />
-                                            <label className="form-check-label" htmlFor="exampleRadios1">
-                                                Default radio
-                                            </label>
-                                        </div>
+                                    <label className="container1">four
+                                        <input type="radio"  name="radio"/>
+                                        <span className="checkmark"></span>
+                                        </label>
                                     </div>
                                 </li>
 
