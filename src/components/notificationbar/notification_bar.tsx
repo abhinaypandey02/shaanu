489c6c49b862efc462daa801f6217d9bf9b20d9b
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-08-24 23:23:44 +0530

    dynamic notification center

--- src/components/notificationbar/notification_bar.tsx
+++ src/components/notificationbar/notification_bar.tsx
@@ -5,6 +5,14 @@ export default function NotificationBar({
 }: {
   currentCarProfile: CarProfile;
 }) {
+  let dueDays = NaN;
+  if (currentCarProfile.insuranceDate) {
+    dueDays =
+      (new Date(currentCarProfile.insuranceDate).getTime() -
+        new Date().getTime()) /
+        86400000 +
+      365;
+  }
   return (
     <div className="container border rounded border-warning rounded-0 p-3 mb-4 text-center">
       <div className="row border-bottom border-warning rounded-0">
@@ -12,13 +20,18 @@ export default function NotificationBar({
       </div>
       <div className="row pt-3">
         <ul className="text-left text-light">
+          {dueDays < 1 && <li>Your Insurance is due!</li>}
+          {dueDays && (
+            <li>Your Insurance is due in {dueDays.toFixed(0)} days!</li>
+          )}
           {currentCarProfile.notifications?.map((text) => (
             <li>{text}</li>
           ))}
           {(!currentCarProfile.notifications ||
-            currentCarProfile.notifications.length === 0) && (
-            <div className="text-warning">NO NEW NOTIFICATION</div>
-          )}
+            currentCarProfile.notifications.length === 0) &&
+            isNaN(dueDays) && (
+              <div className="text-warning">NO NEW NOTIFICATION</div>
+            )}
         </ul>
       </div>
     </div>
