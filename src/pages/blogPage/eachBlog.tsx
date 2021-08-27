b0a52c68a6f27ede4a2b0ce0be4a1f4cf4675fd9
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-08-27 19:50:10 +0530

    dwadd

--- src/pages/blogPage/eachBlog.tsx
+++ src/pages/blogPage/eachBlog.tsx
@@ -3,7 +3,8 @@ import Blog from "../../interfaces/blog";
 
 const EachBlog = ({ blog }: { blog: Blog }) => {
   return (
-    <div className="row">
+    <div className="container-fluid">
+      <div className="row">
       <div className="col-md-4 p-3 d-flex justify-content-center align-items-center bg-warning">
         <img
           src="https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
@@ -11,10 +12,12 @@ const EachBlog = ({ blog }: { blog: Blog }) => {
         />
       </div>
       <div className="col-md-8">
-        <h1 className="font-weight-bold">{blog.title}</h1>
-        <p>{blog.content}</p>
+        <h1 className="font-weight-bold text-light">{blog.title}</h1>
+        <p className='text-light'>{blog.content}</p>
       </div>
     </div>
+    </div>
+    
   );
 };
 
