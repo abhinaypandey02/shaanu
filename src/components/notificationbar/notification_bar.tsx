51529151e3f5c29830f11348756bb7b2a24bad32
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-09-05 23:42:21 +0530

    marquee

--- src/components/notificationbar/notification_bar.tsx
+++ src/components/notificationbar/notification_bar.tsx
@@ -1,44 +1,50 @@
-import { CarProfile } from "../../interfaces/car";
+import { CarProfile } from "../../interfaces/car"
+import Marquee from "react-fast-marquee"
 
 export default function NotificationBar({
-  currentCarProfile,
-}: {
-  currentCarProfile: CarProfile;
+                                            currentCarProfile
+                                        }: {
+    currentCarProfile: CarProfile;
 }) {
-  let dueDays = NaN;
-  if (currentCarProfile.insuranceDate) {
-    dueDays =
-      (new Date(currentCarProfile.insuranceDate).getTime() -
-        new Date().getTime()) /
-        86400000 +
-      365;
-  }
-  return (
-    <div className="container border rounded border-warning rounded-0 p-3 mb-4 text-center">
-      <div className="row border-bottom border-warning rounded-0">
-        <h1 className="text-light mx-auto"> NOTIFICATION CENTER</h1>
-      </div>
-      <div className="row pt-3">
-        <ul className="text-left text-light">
-          {dueDays < 1 && <li>Your Insurance is due!</li>}
-          {dueDays && (
-            <li>Your Insurance is due in {dueDays.toFixed(0)} days!</li>
-          )}
-          {currentCarProfile.notifications?.map((text) => (
-            <li>
-              <div className="marquee">
-                <p>Test</p>
-              </div>
-              {text}
-            </li>
-          ))}
-          {(!currentCarProfile.notifications ||
-            currentCarProfile.notifications.length === 0) &&
-            isNaN(dueDays) && (
-              <div className="text-warning">NO NEW NOTIFICATION</div>
-            )}
-        </ul>
-      </div>
-    </div>
-  );
+    let dueDays = NaN
+    if (currentCarProfile.insuranceDate) {
+        dueDays =
+            (new Date(currentCarProfile.insuranceDate).getTime() -
+                new Date().getTime()) /
+            86400000 +
+            365
+    }
+    return (
+        <div className="container border rounded border-warning rounded-0 p-3 mb-4 text-center">
+            <div className="row border-bottom border-warning rounded-0">
+                <h1 className="text-light mx-auto"> NOTIFICATION CENTER</h1>
+            </div>
+            <div className="row pt-3">
+                <Marquee speed={50} direction="right" gradient={false}>
+                    <div className="text-white">
+                        Welcome to Car Plus! Enjoy!
+                    </div>
+                </Marquee>
+                <ul className="text-left text-light">
+                    {dueDays < 1 && <li>Your Insurance is due!</li>}
+                    {dueDays && (
+                        <li>Your Insurance is due in {dueDays.toFixed(0)} days!</li>
+                    )}
+                    {currentCarProfile.notifications?.map((text) => (
+                        <li>
+                            <div className="marquee">
+                                <p>Test</p>
+                            </div>
+                            {text}
+                        </li>
+                    ))}
+                    {(!currentCarProfile.notifications ||
+                        currentCarProfile.notifications.length === 0) &&
+                    isNaN(dueDays) && (
+                        <div className="text-warning">NO NEW NOTIFICATION</div>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
 }
